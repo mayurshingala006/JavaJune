5110478d452be5c3f931a5400a1b837feb3be56f
mayur9552 <mayur2618006@gmail.com>
2023-11-18 11:25:22 -0500

    Start

---
+++ src/main/java/com/example/JavaJune/JavaJuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaJune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJuneApplication.class, args);
+	}
+
+}
